3ceb6882af2f376d82d794dc16a206983f2b20c0
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-29 23:19:25 -0500

    added a fix for parenting with tags

--- models/index.js
+++ models/index.js
@@ -18,12 +18,12 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-    through: { model: ProductTag, unique: false },
+  through: ProductTag,
 });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsTo(Product, {
-    through: { model: ProductTag, unique: false },
+Tag.belongsToMany(Product, {
+    through: ProductTag,
 });
 
 module.exports = {
