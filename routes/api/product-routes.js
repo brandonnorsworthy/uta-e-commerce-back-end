31c9ea7ddb2294b76637a26d5bb981c1119dfc15
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-29 23:06:02 -0500

    product-routes.js GET requests work

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,14 +4,36 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
 	// find all products
+	try {
+		const data = await Product.findAll({
+			include: [{ model: Category }, { model: Tag }],
+		});
+		if (!data) {
+			res.status(404).json({ message: 'No Product with this id!' });
+		}
+		res.status(200).json(data);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 	// be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async  (req, res) => {
 	// find a single product by its `id`
+	try {
+		const data = await Product.findByPk(req.params.id, {
+			include: [{ model: Category }, { model: Tag }],
+		});
+		if (!data) {
+			res.status(404).json({ message: 'No Product with this id!' });
+		}
+		res.status(200).json(data);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 	// be sure to include its associated Category and Tag data
 });
 
