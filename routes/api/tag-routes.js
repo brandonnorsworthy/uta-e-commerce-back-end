e053d433371507f7f51aa6094e68daa2caf56019
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-29 23:19:40 -0500

    tag-routes.js GET requests work

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,13 +3,35 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
 	// find all tags
+	try {
+		const data = await Tag.findAll({
+			include: { model: Product },
+		});
+		if (!data) {
+			res.status(404).json({ message: 'No Tag with this id!' });
+		}
+		res.status(200).json(data);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 	// be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
 	// find a single tag by its `id`
+	try {
+		const data = await Tag.findByPk( req.params.id, {
+			include: { model: Product },
+		});
+		if (!data) {
+			res.status(404).json({ message: 'No Tag with this id!' });
+		}
+		res.status(200).json(data);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 	// be sure to include its associated Product data
 });
 
