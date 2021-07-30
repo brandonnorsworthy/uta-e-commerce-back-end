5efd6f369e9e5e9db68630e15d3cd88f3f0dedae
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-29 22:59:09 -0500

    category-routes.js GET requests work

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,15 +2,14 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-router.post('/', (req, res) => {
-	// create a new category
-});
 
 router.get('/', async (req, res) => {
 	// find all categories
 	try {
-		const data = await Category.findAll();
-		if (!data){
+		const data = await Category.findAll({
+			include: { model: Product },
+		});
+		if (!data) {
 			res.status(404).json({ message: 'No Category with this id!' });
 		}
 		res.status(200).json(data);
@@ -20,9 +19,24 @@ router.get('/', async (req, res) => {
 	// be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
 	// find one category by its `id` value
+	try {
+		const data = await Category.findByPk(req.params.id, {
+			include: { model: Product },
+		});
+		if (!data) {
+			res.status(404).json({ message: 'No Category with this id!' });
+		}
+		res.status(200).json(data);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 	// be sure to include its associated Products
+});
+
+router.post('/', (req, res) => {
+	// create a new category
 });
 
 router.put('/:id', (req, res) => {
